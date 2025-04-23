7192b5274d9761067fadf20b7bb3d4bfbfdc5c80
devrop <eintrusty@gmail.com>
2025-04-23 19:01:00 +0700

    add discovery

---
+++ rest-base/src/main/java/ropandi/itv/restbase/RestBaseApplication.java
@@ -0,0 +1,13 @@
+package ropandi.itv.restbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBaseApplication.class, args);
+    }
+
+}
--- rest-base/src/test/java/ropandi/itv/restbase/RestBaseApplicationTests.java
+++ rest-base/src/test/java/ropandi/itv/restbase/RestBaseApplicationTests.java
@@ -1,10 +1,10 @@
-package ropandi.itv.gateway;
+package ropandi.itv.restbase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class RestBaseApplicationTests {
 
     @Test
     void contextLoads() {
